3caa4881e482af8b1eefeec556ec1bb15008f7b4
vikas <119844435+Rv229@users.noreply.github.com>
2024-07-09 07:07:00 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,66 @@
+//string example
+let score = "33abc"
+
+//console.log(typeof(score))
+//console.log(typeof score)
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// null expamle
+let scorea = null
+
+//console.log(typeof scorea)
+
+let valueInNumbera = Number(scorea)
+
+//console.log(typeof valueInNumbera)
+//console.log(valueInNumbera)
+
+//undefined example
+let unscore = undefined
+
+//console.log(typeof unscore)
+let valueInNumberb = Number(unscore)
+
+//console.log(typeof valueInNumberb)
+//console.log(valueInNumberb)
+
+//boolean example
+
+let scoreb = true
+
+//console.log(scoreb)
+//console.log(typeof scoreb)
+
+let InNumber = Number(scoreb)
+
+//console.log(typeof InNumber)
+//console.log(InNumber)
+
+
+// "33" => 33
+// "33abc" => NaN(Not A Number)
+// true => 1 & false => 0
+
+
+//Boolean
+let isLoggedIn = "0"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn)
+
+// 0 => true;  1 => true
+// "" => false
+// "anything" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
